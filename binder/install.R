61f569781de88be4104d562956d09e06bca62012
nkanrar <65882575+nkanrar@users.noreply.github.com>
2020-09-22 15:23:30 -0700

    fix_r-base_binder_error

--- binder/install.R
+++ binder/install.R
@@ -1,4 +1,3 @@
-install.packages("- r-base=3.6.1-3bionic")
 install.packages("data.table", type = "source", repos = "https://Rdatatable.gitlab.io/data.table")
 install.packages(c('devtools', 'gam', 'RColorBrewer', 'BiocManager', 'plotly'))
 BiocManager::install(c("scran", "MAST", "slingshot", "ComplexHeatmap", "Seurat", "tradeSeq", "DEsingle"), version="3.10")
