ab21faa3a45916191fe7fa66b4dca407abac4898
nkanrar <65882575+nkanrar@users.noreply.github.com>
2020-09-22 21:42:16 -0700

    Specify R installation location

--- binder/postbuild.sh
+++ binder/postbuild.sh
@@ -1,3 +1,3 @@
-sudo su - -c "R -e \"install.packages("data.table", type = "source", repos = "https://Rdatatable.gitlab.io/data.table")\""
-sudo su - -c "R -e \"install.packages(c('devtools', 'gam', 'RColorBrewer', 'BiocManager', 'plotly'))\""
-sudo su - -c "R -e \"13. BiocManager::install(c("scran", "MAST", "slingshot", "DEsingle"), version="3.10")\""
+sudo su - -c "R -e \"install.packages("data.table", type = "source", repos = "https://Rdatatable.gitlab.io/data.table", lib = "/srv/conda/envs/notebook/lib/R/library")\""
+sudo su - -c "R -e \"install.packages(c('devtools', 'gam', 'RColorBrewer', 'BiocManager', 'plotly'), lib = "/srv/conda/envs/notebook/lib/R/library")\""
+sudo su - -c "R -e \"13. BiocManager::install(c("scran", "MAST", "slingshot", "DEsingle"), version="3.10", lib = "/srv/conda/envs/notebook/lib/R/library")\""
